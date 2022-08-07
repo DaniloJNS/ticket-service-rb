52197fdad11ed62768fe9e0d9895ae363a62414b
DaniloJNS <dan.silva.13.zx@gmail.com>
2022-08-07 13:02:56 -0300

    Setup allowed hosts

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module TicketService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.hosts << ENV['HOST_URL'] || ''
 
     config.secret_key_base = ENV['SECRET_KEY']
     # Configuration for the application, engines, and railties goes here.
