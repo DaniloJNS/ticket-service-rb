9e8dfb4c7479f536487aa8d790e4716822613163
DaniloJNS <dan.silva.13.zx@gmail.com>
2022-08-07 11:27:26 -0300

    Set config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module TicketService
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
     config.hosts << ENV['HOST_URL'] || ''
+
+    config.secret_key_base = ENV['SECRET_KEY']
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
