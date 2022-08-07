82e9253c79622a608e8373ddacb34d7b9ef3f64b
DaniloJNS <dan.silva.13.zx@gmail.com>
2022-08-07 11:13:41 -0300

    Revert "Allowed cloud connect"
    This reverts commit 6bbb8399e34c0e0c3feec8f6b5ec179f054e3767.

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module TicketService
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.hosts << ENV['HOST_URL'] || ''
-
-    config.hosts << "ticket-service-rb-production.up.railway.app"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
