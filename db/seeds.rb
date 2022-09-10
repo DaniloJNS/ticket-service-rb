ebc22ea3ffcfe1f25c120eb4d158aa2ddf3a7683
DaniloJNS <dan.silva.13.zx@gmail.com>
2022-09-09 21:02:22 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+Event.find_or_create_by(name: 'RubyConf+') do |event|
+  event.description = 'O RubyConf existe para promover a conectividade entre profissionais de internet,'\
+    ' estimular a troca de informações e fornecer conteúdos relevantes e de qualidade'\
+                      ' sobre o vasto mundo da tecnologia.'
+end
+
+Event.find_or_create_by(name: 'Ruby Summit') do |event|
+  event.description = 'A Ruby Summit foi criada com propósito de promover integração das pessoas da comunidade ruby'\
+                      ' no Brasil'
+end
+
+Event.find_or_create_by(name: 'Ruby Summit') do |event|
+  event.description = 'A Ruby Summit foi criada com propósito de promover integração das pessoas da comunidade ruby'\
+                      ' no Brasil'
+end
+
+Event.find_or_create_by(name: 'Rails Girls') do |event|
+  event.description = 'Vamos mergulhar no mundo mágico de Ruby on Rails. Você aprenderá a projetar, prototipar'\
+                      ' e codificar com a ajuda de nossos treinadores. Você não precisa de nenhuma experiência'\
+                      ' anterior em codificação. Você precisa do seu próprio laptop, curiosidade e uma pitada '\
+                      'de imaginação!'
+end
